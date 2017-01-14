60c484435e8f8a4d8b5f60b1c0583bc14691b1f8
ihoelscher <iigghh92>
2017-01-14 13:57:49 -0200

    New v.

---
+++ README.md
@@ -0,0 +1,14 @@
+# MIMIC II Waveform Database Analysis with Python
+
+Data analysis on [MIMIC II Waveform Database](https://www.physionet.org/physiobank/database/mimic2wdb/).
+The main objective is to search for a robust Blood Pressure estimator using ECG and PLETH waveforms, but
+there are many other possibilities with the whole data.
+
+## Prerequisites
+
+Anaconda Python 2.7
+
+## Content
+
+Python Scraper
+...
--- goThrough.py
+++ goThrough.py
@@ -1,38 +1,46 @@
-import os
+# A simple scraper to get the directory
+# structure for all records at the MIMIC II Waveform Database.
+# Each intermediate levels will be a dictionary where:
+#   for each key (record), the value is a list of files in the record directory.
+#
+# Example:
+# intermediate level 30 - DBstructure/30.pkl
+# { '3000003': ['3000003.hea', '3000003_001.dat', ..., '3000003n.hea']
+#   ...
+#   '3099997': ['3099997.hea', '3099997_001.dat', ..., '309997n_005.dat']   }
+
+
 from bs4 import BeautifulSoup as bs
 import requests
 
 mimic2wb = "https://www.physionet.org/physiobank/database/mimic2wdb/"
 intermediate_levels = ['%s/' % i for i in range(31, 40)]
 
-def getMimicFiles(html, levels):
+def getMimicFiles(html, level):
+    """ Create the dictionary for a intermediate level """
 
     rec_files = dict()
 
-    for lvl in levels:
-        lvl_link = html+lvl
-        soup = bs(requests.get(lvl_link).text, "lxml")
-        records = [a['href'] for a in soup.find_all('a') if a['href'].startswith(lvl.split('/')[0])]
+    lvl_link = html+level
+    soup = bs(requests.get(lvl_link).text, "lxml")
+    records = [a['href'] for a in soup.find_all('a') if a['href'].startswith(level.split('/')[0])]
 
-        print records[0], records[-1]
+    for rec in records:
+        rec_link = lvl_link+rec
+        soup = bs(requests.get(rec_link).text, "lxml")
+        files = [a['href'] for a in soup.find_all('a') if a['href'].startswith(level.split('/')[0])]
 
-
-        for rec in records:
-            rec_link = lvl_link+rec
-            soup = bs(requests.get(rec_link).text, "lxml")
-            files = [a['href'] for a in soup.find_all('a') if a['href'].startswith(lvl.split('/')[0])]
-
-            rec_files[rec.split('/')[0]] = files
+        rec_files[rec.split('/')[0]] = files
 
     return rec_files
 
 import pickle
 def saveMimicStructure(dict, name):
-    with open(name+'.pkl', 'wb') as f:
+    with open('DBstructure/'+name+'.pkl', 'wb') as f:
         pickle.dump(dict, f, pickle.HIGHEST_PROTOCOL)
 
 for i in intermediate_levels:
-    files = getMimicFiles(mimic2wb, intermediate_levels)
+    files = getMimicFiles(mimic2wb, i)
     saveMimicStructure(files, i.split('/')[0])
 
 print 'ok'
